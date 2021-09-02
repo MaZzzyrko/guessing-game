52cc132372eea277636f7532034d90f97dc7478b
Evgeny <hatehateme@bk.ru>
2021-09-02 12:00:46 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.cent = Math.round ((this.max - this.min) / 2) + this.min;
+                return this.cent;
     }
 
     lower() {
-
+        this.setRange (this.min, this.cent);
     }
 
     greater() {
-
+        this.setRange (this.cent, this.max)
     }
 }
 
